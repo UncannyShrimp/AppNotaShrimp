024566183fca17aebcb432f2f7dc38c12ff19fea
Andres Jimenez <AF_JIMENEZ_SANTAMARIA@HOTMAIL.COM>
2026-03-10 23:48:41 -0500

    Se agrego el modelo de la clase Notas

---
+++ AppNotaShrimp/Models/Notas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNotaShrimp.Models
+{
+    public class Notas
+    {
+        public required int Id { get; set; }
+        public  required string Title { get; set; }
+        public string? Content { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime UpdatedAt { get; set; }
+        public required bool IsFavorite { get; set; } 
+
+
+    }
+}
